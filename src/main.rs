968afa226007d5a91d3f3d9bb99ba327c0bef68f
Aaditya Bhatnagar <11aadityab@gmail.com>
2025-09-09 00:52:48 +0400

    First iteration of MLP done

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,31 @@
+// main.rs
 mod network;
-use network::InputLayer;
-use network::Layer;
+use network::{InputLayer, Layer, WeightsType};
 
 fn main() {
     let _learning_rate = 0.1;
     let _y_target = 1.00;
-    let mut inputs= InputLayer::new(vec![0.0, 1.0]);
-    let weights = vec![vec![1.0, 3.0], vec![2.0, 4.0]];
-    let biases = vec![1.0, 2.0];
-    let mut hidden = Layer::new(weights, biases);
-    let hidden_outputs = hidden.forward_prop(&mut inputs);
 
-    println!("{:?}", hidden_outputs);
+    // Inputs
+    let inputs = InputLayer::new(vec![0.0, 1.0]);
+
+    // Input -> Hidden
+    let weights_1 = WeightsType::Multiple(vec![vec![1.0, 3.0], vec![2.0, 4.0]]);
+    let biases_1 = vec![1.0, 2.0];
+    let mut input_to_hidden = Layer::new(weights_1, biases_1);
+    let hidden_outputs = InputLayer::new(input_to_hidden.forward_prop(&inputs));
+
+    println!("Hidden layer outputs: {:?}", hidden_outputs);
+
+    // Hidden -> Output
+    let weights_2 = WeightsType::Single(vec![5.0, 6.0]);
+    // let weights_2 = WeightsType::Multiple(vec![vec![5.0], vec![6.0]]);
+    let biases_2 = vec![3.0];
+    let mut hidden_to_output = Layer::new(weights_2, biases_2);
+    let y = hidden_to_output.forward_prop(&hidden_outputs);
+
+    println!("Final output: {:?}", y);
+    let error = _y_target - y[0];
+    println!("Error: {}", error);
+
 }
-
-// fn read_input<T: std::str::FromStr>(msg: &str) -> T{
-//     loop {
-//         let mut input = String::new();
-//         println!("{}", msg);
-//         io::stdin()
-//             .read_line(&mut input)
-//             .expect("Should never fail to read input");
-//         match input.trim().parse::<T>() {
-//             Ok(val) => return val,
-//             Err(e) => println!("Error: Invalid Input"),
-//         }
-//     }
-// }
--- src/network.rs
+++ src/network.rs
@@ -1,45 +1,65 @@
-//                                    b1 
+//                                    b1
 // Input (0) -> w1 \                  |
-//                  -> Hidden Neuron (h1) -> w5    
+//                  -> Hidden Neuron (h1) -> w5
 // Input (1) -> w3 /                            \
 //                                               --> Output Neuron (y) Error = y^ - y
 // Input (0) -> w2 \                            /                   |
 //                  -> Hidden Neuron (h2) -> w6                     b3
-// Input (1) -> w4 /                  |                             
-//                                    b2                            
-fn sigmoid(x: f64) -> f64{
-    1.0/(1.0 + (-x).exp())
+// Input (1) -> w4 /                  |
+//                                    b2
+fn sigmoid(x: f64) -> f64 {
+    1.0 / (1.0 + (-x).exp())
 }
-pub struct InputLayer{
+#[derive(Debug)]
+pub struct InputLayer {
     pub inputs: Vec<f64>,
 }
 
-impl InputLayer{
-    pub fn new(inputs: Vec<f64>) -> Self{ 
+impl InputLayer {
+    pub fn new(inputs: Vec<f64>) -> Self {
         InputLayer { inputs }
     }
 }
 
-pub struct Layer{
-    pub weights: Vec<Vec<f64>>, // [1, 2, 3, 4, 5, 6]
-    pub biases: Vec<f64>, // [1, 2, 3]
+pub enum WeightsType {
+    Single(Vec<f64>),
+    Multiple(Vec<Vec<f64>>),
+}
+pub struct Layer {
+    pub weights: WeightsType, // [1, 2, 3, 4, 5, 6]
+    pub biases: Vec<f64>,      // [1, 2, 3]
 }
 
-impl Layer{
-    pub fn new(weights: Vec<Vec<f64>>, biases: Vec<f64>) -> Self{
+impl Layer {
+    pub fn new(weights: WeightsType, biases: Vec<f64>) -> Self {
         Layer { weights, biases }
     }
 
-    pub fn forward_prop(&mut self, neuron_inputs: &mut InputLayer) -> Vec<f64> {
-        let mut outputs= Vec::new();
+    pub fn forward_prop(&mut self, neuron_inputs: &InputLayer) -> Vec<f64> {
+        let mut outputs = Vec::new();
 
-        for (neuron_weights, neuron_bias) in self.weights.iter().zip(self.biases.iter()) {
-            let h: f64 = neuron_weights.iter()
-                .zip(neuron_inputs.inputs.iter())
-                .map(|(w, i)| w * i)
-                .sum::<f64>() + neuron_bias;
-            outputs.push(sigmoid(h));
+        match &self.weights {
+            WeightsType::Single(weights_vec) => {
+                let h = weights_vec
+                    .iter()
+                    .zip(neuron_inputs.inputs.iter())
+                    .map(|(w, i)| w * i)
+                    .sum::<f64>()
+                    +self.biases[0];
+                outputs.push(sigmoid(h));
+            }
+            WeightsType::Multiple(weights_matrix) => {
+                for (neuron_weights, neuron_bias) in weights_matrix.iter().zip(self.biases.iter()) {
+                    let h: f64 = neuron_weights
+                        .iter()
+                        .zip(neuron_inputs.inputs.iter())
+                        .map(|(w, i)| w * i)
+                        .sum::<f64>()
+                        + neuron_bias;
+                    outputs.push(sigmoid(h));
+                }
+            }
         }
         outputs
-    }    
+    }
 }
